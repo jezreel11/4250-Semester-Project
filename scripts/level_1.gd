d306264bf5dcd16cb9c528f22f1fee894362f6b7
gahbaha <acklinshaun2@gmail.com>
2026-04-13 17:47:23 -0400

    Add wave spawning with timer

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,16 +1,40 @@
 extends Node2D
 
-# Prereloading the enemy 
+# Preloading the enemy 
 @export var enemy_scene = preload("res://assets/Animations/LancerRunning.tscn")
 @export var enemy_attack = preload("res://assets/Animations/LancerAttacking.tscn")
+
 # Preload Towers for placement
 @onready var tower_scene = preload("res://scenes/towers/Tower.tscn")
 
 # Tracks all placed towers in order
 var placed_towers = []
 
+# -- here is the wave management variables --
+
+# this here is the wave size controls how many enemies spawn in one wave (can be edited in Godot but im not too experienced with that yet so i just set it here for now) - deshaun
+@export var wave_size : int = 3
+@export var spawn_interval : float = 1.0 # Time in seconds between enemy spawns
+
+var enemies_spawned : int = 0
+var spawn_timer : Timer
+
+
 func _ready():
-	spawn_enemy()
+	# Initialize the spawn timer
+	spawn_timer = Timer.new()
+	spawn_timer.wait_time = spawn_interval
+	spawn_timer.one_shot = false
+	spawn_timer.connect("timeout", Callable(self, "_on_spawn_timer_timeout"))
+	add_child(spawn_timer)
+	spawn_timer.start()
+
+func _on_spawn_timer_timeout():
+	if enemies_spawned < wave_size:
+		spawn_enemy()
+		enemies_spawned += 1
+	else:		
+		spawn_timer.stop() # Stop the timer when the wave is complete
 
 func spawn_enemy():
 	var enemy = enemy_scene.instantiate()
