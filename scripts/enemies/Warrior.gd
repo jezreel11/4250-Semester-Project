a87c79830fd289e38e70f69a4e83bbba5a721b85
jezreel11 <rodriguezjezreel435@gmail.com>
2026-04-21 18:45:38 -0400

    Added own logic for warrior

---
+++ scripts/enemies/Warrior.gd
@@ -0,0 +1,13 @@
+extends "res://scripts/enemies/enemypath.gd"
+class_name WarriorEnemy
+
+
+func _ready():
+	max_health = 160
+	attack_damage = 25
+	running_animation_name = &"Running"
+	attack_animation_name = &"Attacking"
+	idle_animation_name = &"Running"
+	use_embedded_attack_animation = true
+	use_embedded_idle_animation = true
+	super()
